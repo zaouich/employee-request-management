67f575af90e02487fe04f3d737b0bf0154f626af
zaouich <121829336+zaouich@users.noreply.github.com>
2023-03-21 21:48:23 +0000

    add signUp and forgot password

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import ForgotPassword from "./components/forgotPassword";
 import Login from "./components/login";
 import Navbar from "./components/navbar";
+import SignUp from "./components/signUp";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
        <Navbar/>
       <Routes>
         <Route path = "/login" element={<Login/>}/>
+        <Route path = "/signUp" element={<SignUp/>}/>
+        <Route path = "/forgotPassword" element={<ForgotPassword/>}/>
       </Routes>
     </div>
   );
